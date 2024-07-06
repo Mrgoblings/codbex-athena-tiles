ae35f679bb67127bca72427d77f64416b73c53a8
MrGoblings <mrgoblings@gmail.com>
2024-07-06 08:49:48 +0000

    add: functionality for floating number display

--- tiles.js
+++ tiles.js
@@ -8,7 +8,8 @@ angular.module('applicationTiles', [])
                 title: '@',
                 subtitle: '@?',
                 numericData: '=',
-                footerText: '@?'
+                footerText: '@?',
+                isFloatingNumber: '<?'
             },
             controller: ['$scope', function ($scope) {
                 if (!$scope.footerText) {
